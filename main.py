dcc7be9d23d63e64083de59dc2266703b74d5ba0
Mestre Pedro <pedrooliveirasouzaleal@gmail.com>
2024-06-18 23:53:56 -0300

    Criação do projeto

---
+++ main.py
@@ -0,0 +1,27 @@
+
+'UMA ÚNICA VEZ'
+#instalar o git
+#git init - inicia o repositório.
+#git config --global user.name "Mestre Pedro"
+#git config --global user.email pedrooliveirasouzaleal@gmail.com
+#git branch -m master main
+
+'AO ATUALIZAR O PROJETO'
+#git add .
+#git commit -m "Criação do projeto"
+#git push origin main
+
+
+'ACÕES DE CONTROLE'   
+#git log - mostra o histórico de commit
+#git checkout *** - volta em um commit especifico
+#git checkout main - volta para o topo da árvore
+#git switch - faz ou desfaz a operação
+
+#git remote add origin link do coisa
+
+import os
+os.system("cls")
+
+print("Bem-vindo ao Projeto Asteróide!")
+
